f9aa2003f3cddfb154098ae8cffa73d36283f583
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-03-08 16:40:07 +0100

    add mapping out of posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import style from './App.module.css'
 
+import * as postAPI from './data/postController'
+import { posts } from './data/data'
+
+function Post({
+  userId,
+  id,
+  title,
+  body,
+  className
+}) {
+  return (
+    <div className={className}>
+      <div>
+        <h4>{title}</h4>
+        <p>{body}</p>
+      </div>
+      <div>
+        <h6>User: {userId} <span>Post: {id}</span></h6>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className={`${style.App} standard`}>
-      <h1>Hello World</h1>
+      {
+        posts.slice(0, 5).map((post) => <Post className={style.post} {...post} />)
+      }
     </div>
   )
 }
